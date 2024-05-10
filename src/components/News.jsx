f33fb19a73eda96f58789f8872ca3b248b310ce1
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2024-05-10 12:02:32 +0545

    add a spinner for News component

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,20 +1,44 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { ImSpinner9 } from "react-icons/im";
 
 const News = () => {
   const [news, setNews] = useState([]);
   const [articleNum, setArticlesNum] = useState(3);
+  const [spinner, setSpinner] = useState(false);
+  const [error, setError] = useState(false);
+
+  async function loadNews() {
+    setError("");
+    setSpinner(true);
+    try {
+      const response = await fetch(
+        "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch the news!");
+      }
+      const data = await response.json();
+      setNews(data.articles);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setSpinner(false);
+    }
+  }
 
   useEffect(() => {
-    fetch("https://saurav.tech/NewsAPI/top-headlines/category/business/us.json")
-      .then((res) => res.json())
-      .then((data) => setNews(data.articles));
+    loadNews();
   }, []);
 
   return (
     <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2">
       <h4 className="font-bold text-lg px-4">Whats happening...</h4>
+
+      {spinner && <ImSpinner9 className="animate-spin text-blue-300 w-full" />}
+      {error ? <p className="text-red-300 text-sm px-4">{error}</p> : ""}
+
       {news.slice(0, articleNum).map((article) => (
         <div key={article.url}>
           <a href={article.url} target="_blank">
@@ -31,12 +55,22 @@ const News = () => {
           </a>
         </div>
       ))}
-      <button
-        onClick={() => setArticlesNum(articleNum + 3)}
-        className="text-blue-300 hover:text-blue-400 text-sm pl-4 pb-3"
-      >
-        Load more
-      </button>
+
+      {error ? (
+        <button
+          onClick={() => loadNews()}
+          className="text-red-300 hover:text-red-400 text-sm pl-4 pb-3 flex gap-1 items-center"
+        >
+          Reload <ImSpinner9 />
+        </button>
+      ) : (
+        <button
+          onClick={() => setArticlesNum(articleNum + 3)}
+          className="text-blue-300 hover:text-blue-400 text-sm pl-4 pb-3"
+        >
+          Load more
+        </button>
+      )}
     </div>
   );
 };
