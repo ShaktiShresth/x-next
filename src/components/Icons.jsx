60dbcdb7b45ce65619368b639127caf61a93a5fb
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2024-05-13 10:29:02 +0545

    install & implement recoil

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -18,12 +18,16 @@ import {
 } from "react-icons/hi";
 import { app } from "../firebase";
 import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { modalState } from "@/atom/modalAtom";
 
 const Icons = ({ id, uid }) => {
   const { data: session } = useSession();
   const [isLiked, setIsLiked] = useState(false);
   const [likes, setLikes] = useState([]);
   const db = getFirestore(app);
+
+  const [open, setOpen] = useRecoilState(modalState);
 
   const likePost = async () => {
     if (session) {
@@ -71,7 +75,10 @@ const Icons = ({ id, uid }) => {
 
   return (
     <div className="flex justify-start gap-5 py-2 text-gray-500">
-      <HiOutlineChat className="size-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100" />
+      <HiOutlineChat
+        onClick={() => setOpen(!open)}
+        className="size-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100"
+      />
 
       <div className="flex items-center">
         {isLiked ? (
