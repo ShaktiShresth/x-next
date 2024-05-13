7ce682ccbecf649c4e50fbf4c51ee85999838b84
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2024-05-13 16:11:45 +0545

    add like functionality to Comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,77 @@
 "use client";
 
-import { HiDotsHorizontal } from "react-icons/hi";
+import { app } from "@/firebase";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getFirestore,
+  onSnapshot,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+import { signIn, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { HiDotsHorizontal, HiHeart, HiOutlineHeart } from "react-icons/hi";
 
-const Comment = ({ comment, id }) => {
+const Comment = ({ comment, commentId, originalPostId }) => {
+  const { data: session } = useSession();
+  const db = getFirestore(app);
+
+  const [isLiked, setIsLiked] = useState(false);
+  const [likes, setLikes] = useState([]);
+
+  const likePost = async () => {
+    if (session) {
+      if (isLiked) {
+        await deleteDoc(
+          doc(
+            db,
+            "posts",
+            originalPostId,
+            "comments",
+            commentId,
+            "likes",
+            session?.user?.uid
+          )
+        );
+      } else {
+        await setDoc(
+          doc(
+            db,
+            "posts",
+            originalPostId,
+            "comments",
+            commentId,
+            "likes",
+            session.user.uid
+          ),
+          {
+            username: session.user.username,
+            timestamp: serverTimestamp(),
+          }
+        );
+      }
+    } else {
+      signIn();
+    }
+  };
+
+  useEffect(() => {
+    onSnapshot(
+      collection(db, "posts", originalPostId, "comments", commentId, "likes"),
+      (snapshot) => {
+        setLikes(snapshot.docs);
+      }
+    );
+  }, [db]);
+
+  useEffect(() => {
+    setIsLiked(
+      likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+    );
+  }, [likes]);
+
   return (
     <div className="flex p-3 pl-10 border-b border-gray-200 hover:bg-gray-50">
       <img
@@ -20,6 +89,25 @@ const Comment = ({ comment, id }) => {
         </div>
 
         <p className="text-gray-800 text-xs my-3">{comment?.comment}</p>
+
+        <div className="flex items-center">
+          {isLiked ? (
+            <HiHeart
+              onClick={likePost}
+              className="size-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 text-red-600 hover:text-red-500 hover:bg-red-100"
+            />
+          ) : (
+            <HiOutlineHeart
+              onClick={likePost}
+              className="size-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
+            />
+          )}
+          {likes.length > 0 && (
+            <span className={`text-xs ${isLiked && "text-red-600"}`}>
+              {likes.length}
+            </span>
+          )}
+        </div>
       </div>
     </div>
   );
